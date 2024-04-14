e627251a90d294435e45e0383cf7dc5c20cabe51
nam-dao <namdao08042003@gmail.com>
2024-04-14 17:58:57 +0700

    State - Kiem soat data ung dung React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,16 @@
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "Nam",
+    address: "Hoi Dan IT",
+    age: 26,
+  };
+
   //JSX
   render() {
     return (
       <div>
-        My first Component
-        {Math.random()}
+        My name is {this.state.name} and I'm from {this.state.address}
       </div>
     );
   }
