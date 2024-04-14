00719f864f367a1bec8cf58a337c89c048fcd2b3
nam-dao <namdao08042003@gmail.com>
2024-04-14 18:23:26 +0700

    DOM Event - Xu Ly Su Kien Voi React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,11 +6,20 @@ class MyComponent extends React.Component {
     age: 26,
   };
 
+  handleClick(event) {
+    console.log("Click me my button");
+    console.log("My name is ", this.state.name)
+  }
+  handleOnMoverOver(event) {
+    console.log(event.pageX)
+  }
   //JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
+        <button onMouseOver = {this.handleOnMoverOver}>Hover Me</button>
+        <button onClick = {this.handleClick}>Click Me</button>
       </div>
     );
   }
