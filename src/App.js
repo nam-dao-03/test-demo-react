72eca28c4bb9faa2c6bfde374b6436ab5dc8f09e
nam-dao <namdao08042003@gmail.com>
2024-04-16 14:50:20 +0700

    Dieu huong trang voi Link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
 import "./App.scss";
-import Header from './components/Header/Header'
+import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 const App = () => {
-
   return (
     <div className="app-container">
       <Header />
+      <div>
+        test Link
+        <div>
+          <button>
+            <Link to="/users">go to User page</Link>
+          </button>
+          <button>
+            <Link to="/admins">go to Admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
