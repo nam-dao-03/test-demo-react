101b7a80777d76199082ac514752563991eece2a
nam-dao <namdao08042003@gmail.com>
2024-04-16 18:23:11 +0700

    Design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,6 +1,13 @@
+import videoHomePage from '../../assets/video-homepage.mp4'
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
+            <video autoPlay muted loop>
+                <source
+                    src = {videoHomePage}
+                    type = "video/mp4"
+                 />
+            </video>
             home page component
         </div>
     );
